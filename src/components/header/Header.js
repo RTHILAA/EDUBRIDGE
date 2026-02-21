8254edf63085ac1ac9519495fccf7196e59dbdf6
anaswins350-ai <anaswins35@gmail.com>
2026-02-21 19:06:04 +0000

    feat: add Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+import { ArrowRight } from 'lucide-react';
+
+function Header() {
+  return (
+    <div className='header'>
+        <nav>
+        <h3 className='logo'>EduBridge</h3>
+        <ul>
+            <li><a href="#!">Home</a></li>
+            <li><a href="#!">Product</a></li>
+            <li><a href="#!">Pricing</a></li>
+            <li><a href="#!">Contact</a></li>
+        </ul>
+        <div className="account">
+            <a href="#!" className='btn-login'>Login</a>
+            <a href="#!" className='btn-join'>Join Now <ArrowRight size={16} /></a>
+        </div>
+        </nav>
+    </div>
+  ) 
+}
+
+export default Header
