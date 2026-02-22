020de2988e35b51f7917933294ee5385941ca58d
anaswins350-ai <anaswins35@gmail.com>
2026-02-22 23:18:00 +0000

    Add main component structure

---
+++ src/components/main/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container'>
+        <div className="content">
+            <span>Join Us</span>
+            <h1>Best Learning Opportunities</h1>
+            <p>We know how large objects will act, but things on a small scale just do not act that way.</p>
+            <a href="#!" className="btn-quote">Get Quote Now</a>
+            <a href="#!" className="btn-learn">Learn More</a>
+        </div>
+    </div>
+  )
+}
+
+export default Home
