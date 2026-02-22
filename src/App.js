c3078a5d4f40e551004a81dc0de33bd9688d5433
anaswins350-ai <anaswins35@gmail.com>
2026-02-22 23:16:02 +0000

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer />
     </>
   )
 }
