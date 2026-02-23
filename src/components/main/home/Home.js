f812ed15b9fe8d241beb4b76719c52a45e7079a3
anaswins350-ai <anaswins35@gmail.com>
2026-02-23 19:50:56 +0000

    feat: update Home component

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -3,12 +3,14 @@ import React from 'react'
 function Home() {
   return (
     <div className='container'>
-        <div className="content">
+        <div className="content-header">
             <span>Join Us</span>
             <h1>Best Learning Opportunities</h1>
             <p>We know how large objects will act, but things on a small scale just do not act that way.</p>
+            <div className="btn-quote-learn">
             <a href="#!" className="btn-quote">Get Quote Now</a>
             <a href="#!" className="btn-learn">Learn More</a>
+            </div>
         </div>
     </div>
   )
