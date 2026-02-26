bd8b48ab6bed86e93a5041cd3d24cc6a708533bb
anaswins350-ai <anaswins35@gmail.com>
2026-02-26 07:47:23 +0000

    Update Home component

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from './Card'
+import CardData from './CardData'
 
 function Home() {
   return (
     <div className='container'>
-        <div className="content">
+        <div className="content-header">
             <span>Join Us</span>
             <h1>Best Learning Opportunities</h1>
             <p>We know how large objects will act, but things on a small scale just do not act that way.</p>
@@ -11,6 +13,17 @@ function Home() {
             <a href="#!" className="btn-quote">Get Quote Now</a>
             <a href="#!" className="btn-learn">Learn More</a>
             </div>
+             </div>
+            <div className="content-cards">
+            {CardData.map((card) => (
+                <Card 
+                    key={card.id}
+                    icon={card.icon}
+                    title={card.title}
+                    description={card.description}
+                    color={card.color}
+                />
+            ))}
         </div>
     </div>
   )
