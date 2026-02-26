311f79978537aa91ddda15a7023019b498268da1
anaswins350-ai <anaswins35@gmail.com>
2026-02-26 07:46:56 +0000

    Add Card component and Data

---
+++ src/components/main/home/CardData.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BookOpen, ChartLine, GraduationCap } from 'lucide-react';
+
+
+const CardData = [
+    {
+        icon: <BookOpen />,
+        title: "Books Library",
+        description: "The gradual accumulation of information about atomic and small-scale behaviour...",
+        color: "#E74040"
+    },
+    {
+        icon: <ChartLine />,
+        title: "Market Analysis",
+        description: "The gradual accumulation of information about atomic and small-scale behaviour...",
+        color: "#2DC071"
+    },
+    {
+        icon: <GraduationCap />,
+        title: "50+ Online Courses",
+        description: "The gradual accumulation of information about atomic and small-scale behaviour...",
+        color: "#B2E3FF"
+    }
+
+]
+
+export default CardData
