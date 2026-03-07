3e7b58d1cc4b62422e7f8853237cf0ffbc4cb231
anaswins350-ai <anaswins35@gmail.com>
2026-03-07 07:46:34 +0000

    refactor: update App.js for new structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import './App.css'
-import Header from './components/header/Header';
-import Main from './components/main/Main';
+import "./styles/App.css"
+import Header from './components/layout/Header/Header';
+import Home from './sections/Home/Home';
+import Courses from './sections/Courses/Courses';
+import Reviews from './sections/Reviews/Reviews';
+import JoinUs from './sections/JoinUs/JoinUs';
+import Footer from './components/layout/Footer/Footer';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Home />
+      <Courses />
+      <Reviews />
+      <JoinUs />
+      <Footer />
     </>
   )
 }
