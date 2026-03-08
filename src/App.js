514064ae5d95b5adba939701655959dd8f03541c
anaswins350-ai <anaswins35@gmail.com>
2026-03-08 23:58:59 +0000

    refactor: update App.js for new structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css"
 import Header from './components/layout/Header/Header';
-import Home from './sections/Home/Home';
-import Courses from './sections/Courses/Courses';
-import Reviews from './sections/Reviews/Reviews';
-import JoinUs from './sections/JoinUs/JoinUs';
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
 import Footer from './components/layout/Footer/Footer';
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Courses />
-      <Reviews />
-      <JoinUs />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
